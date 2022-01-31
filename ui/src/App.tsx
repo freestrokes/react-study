6d32e4284d5b2979af1405c322ec66833d7fbb5b
freestrokes <freestrokes.hk@gmail.com>
2022-01-31 17:48:30 +0900

    [ui] #master Update App.tsx

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,11 +1,11 @@
-import React from "react";
-import { Switch, Route, Link } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
+import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 
-import AddTutorial from "./components/AddTutorial";
-import Tutorial from "./components/Tutorial";
-import TutorialsList from "./components/TutorialsList";
+import AddTutorial from './components/AddTutorial';
+import Tutorial from './components/Tutorial';
+import TutorialsList from './components/TutorialsList';
 
 function App() {
   return (
